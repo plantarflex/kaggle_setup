ab267160eb65407983955fc4c0dd47d4abb4a6cb
plantarflex <plantarflex@naver.com>
2020-12-20 16:46:03 +0900

    add variables for gpu shell

--- run_gpu.sh
+++ run_gpu.sh
@@ -1,9 +1,12 @@
 mkdir -p input && mkdir -p output
 TIMESTAMP=$(date '+%Y%m%d%H%M%S')
+docker rm -f jupyter_gpu
 nvidia-docker run -it \
+  --name jupyter_gpu \
+  --restart always \
   -v $PWD/.jupyter:/root/.jupyter \
   -v $PWD/input:/input \
   -v $PWD/output/$TIMESTAMP:/home \
   -p 8080:8080 \
   kaggle/python-gpu-build:latest \
-  /bin/bash
+  /bin/bash -c './run_jupyter.sh'
