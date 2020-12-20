8647b81388f9e54a622deb0a6aafc12a0b4c8a96
plantarflex <plantarflex@naver.com>
2020-12-20 16:43:58 +0900

    add container name and let restart always

--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
 mkdir -p input && mkdir -p output
 TIMESTAMP=$(date '+%Y%m%d%H%M%S')
-docker run -it \
+docker rm -f jupyter
+docker run -itd \
+  --name jupyter \  
+  --restart always \
   -v $PWD/.jupyter:/root/.jupyter \
   -v $PWD/input:/input \
   -v $PWD/output/$TIMESTAMP:/home \
   -p 8080:8080 \
   kaggle/python-build:latest \
-  /bin/bash
+  /bin/bash -c './run_jupyter.sh'
